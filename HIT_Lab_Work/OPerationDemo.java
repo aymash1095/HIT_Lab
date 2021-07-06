69425924d8f89f64cdfcdd200bdbf198ee6e33b5
aymash1095 <86283541+aymash1095@users.noreply.github.com>
2021-07-06 16:30:52 +0530

    Add files via upload

---
+++ HIT_Lab_Work/OPerationDemo.java
@@ -0,0 +1,18 @@
+package HIT_Lab_Work;
+
+import java.util.Scanner;
+
+public class OPerationDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Enter Num1 ");
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter Number a");
+		int a=scan.nextInt();
+		System.out.println("Enter Number b");
+		int b=scan.nextInt();
+		int sum=a+b;
+		System.out.println("addition of a and b is : "+sum);
+	}
+
+}
